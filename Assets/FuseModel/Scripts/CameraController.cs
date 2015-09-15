9c5fcd9eb1e6317c61e2fae3dbd08d990f6b93ba
Michael Brunson <mbrunson94@gmail.com>
2015-09-14 22:01:32 -0400

    Initial proof-of-concept commit

---
+++ Assets/FuseModel/Scripts/CameraController.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform target;
+    public float lookSmooth = 0.09f;
+    public Vector3 offsetFromTarget = new Vector3(0, 1, -10);
+    public float roll = 0;
+
+
+    Vector3 destination = Vector3.zero;
+    SideScrollCharacterController character;
+
+    public float Roll
+    {
+        get { return roll; }
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public void SetCameraTarget(Transform t)
+    {
+        target = t;
+
+        if (target != null)
+        {
+            if (target.GetComponent<SideScrollCharacterController>())
+            {
+                character = target.GetComponent<SideScrollCharacterController>();
+            }
+            else
+                Debug.LogError("The camera's target needs a SideScrollCharacterController.");
+        }
+        else
+            Debug.LogError("The camera needs a target.");
+    }
+	
+    // Update is called once per frame
+	void Update () {
+        //moving
+        MoveToTarget();
+        //rotating
+        LookAtTarget();
+        //re-orient
+        Reorient();
+	}
+
+    private void Reorient()
+    {
+
+    }
+
+    void MoveToTarget()
+    {
+        destination = target.position + offsetFromTarget;
+        transform.position = destination;
+    }
+
+    void LookAtTarget()
+    {
+
+    }
+}
---
+++ Assets/FuseModel/Scripts/SideScrollCharacterController.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class SideScrollCharacterController : MonoBehaviour
+{
+
+    public float inputDeadzone = 0.1f;
+    public float forwardVel = 1;
+
+    private float forwardInput;
+    private Rigidbody rBody;
+    private Animator animator;
+    private bool grounded;
+    private float distToGround = 0;
+    private bool facingForward = true;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (GetComponent<Rigidbody>())
+        {
+            rBody = GetComponent<Rigidbody>();
+        }
+        else
+        {
+            Debug.LogError("The character needs a rigidBody.");
+        }
+
+        if (GetComponent<Animator>())
+        {
+            animator = GetComponent<Animator>();
+        }
+        else
+        {
+            Debug.LogError("The character does not have an Animator.");
+        }
+        forwardInput = 0;
+    }
+
+    void GetInput()
+    {
+        forwardInput = Input.GetAxis("Horizontal");
+
+        if(facingForward && forwardInput < 0)
+        {
+            transform.rotation *= Quaternion.AngleAxis(180, Vector3.up);
+            facingForward = false;
+        }
+        else if (!facingForward && forwardInput > 0)
+        {
+            transform.rotation *= Quaternion.AngleAxis(180, Vector3.up);
+            facingForward = true;
+        }
+
+        forwardInput = Mathf.Abs(forwardInput);
+
+        if(Input.GetButtonUp("Jump"))
+        {
+            rBody.position -= Physics.gravity.normalized;
+            Quaternion delta = facingForward ? Quaternion.Euler(-90, 0, 0) : Quaternion.Euler(90, 0, 0);
+            Quaternion worldDelta = Quaternion.Euler(0, 0, 90);
+            Physics.gravity = worldDelta * Physics.gravity;
+            rBody.rotation *= delta;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+    }
+
+    void FixedUpdate()
+    {
+        if (animator)
+        {
+            CheckIfGrounded();
+            animator.SetFloat("Speed", forwardInput*3);
+            animator.SetBool("Grounded", grounded);
+            animator.SetFloat("AirVelocity", rBody.velocity.y);
+        }
+        Run();
+    }
+
+    private void CheckIfGrounded()
+    {
+        grounded = Physics.Raycast(rBody.position - Physics.gravity.normalized*.05f, Physics.gravity.normalized, distToGround + 0.1f);
+    }
+
+    void Run()
+    {
+        if (Mathf.Abs(forwardInput) > inputDeadzone)
+        {
+            rBody.position += transform.forward * forwardInput * forwardVel * Time.deltaTime; //new Vector3(forwardInput * forwardVel, rBody.velocity.y, rBody.velocity.z);
+        }
+        else
+        {
+            //rBody.velocity = Vector3.zero; //new Vector3(0, rBody.velocity.y, rBody.velocity.z);
+        }
+    }
+}
