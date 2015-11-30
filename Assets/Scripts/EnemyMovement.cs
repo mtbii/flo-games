b463b2716c8b65f9447b86c07505281b7306b22c
Steven Spitzform <spitzfor@email.sc.edu>
2015-11-30 00:07:31 -0500

    Near-empty test enemy added.  Uses CharacterController but may need RigidBody.

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour {
+
+    private float speed = 3f;
+    private float rotateSpeed = 3f;
+    private CharacterController cont;
+    private Vector3 simGravity;
+
+    void Start()
+    {
+        simGravity = new Vector3(0, -9.8f, 0);
+    }
+
+    void Update()
+    {
+        cont = GetComponent<CharacterController>();
+        cont.SimpleMove(simGravity);
+    }
+}
