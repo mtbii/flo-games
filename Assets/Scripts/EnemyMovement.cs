040ffdafd2fb58ebaabee036aaa6ed11ecde6003
Steven Spitzform <spitzfor@email.sc.edu>
2015-11-30 23:13:35 -0500

    More work on getting enemy to follow/chase player

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -3,19 +3,116 @@ using System.Collections;
 
 public class EnemyMovement : MonoBehaviour {
 
-    private float speed = 3f;
-    private float rotateSpeed = 3f;
+    //public int speed = 5;
+    //public float gravity = 20;
+    //public Vector3 moveSpeed;
+    //public Transform target;
+
+    //void Update()
+    //{
+    //    Move();
+    //}
+
+    //void Move()
+    //{
+    //    CharacterController character = GetComponent<CharacterController>();
+
+    //    if(character.isGrounded)
+    //    {
+    //        Vector3 delta = target.transform.position - transform.position;
+    //        //delta.y = 0;
+    //        delta.Normalize();
+    //        transform.forward = delta;
+    //        moveSpeed = delta * speed;
+    //    }
+
+    //    moveSpeed.y -= gravity * Time.deltaTime;
+    //    character.Move(moveSpeed * Time.deltaTime);
+    //}
+
+    //public float speed = 6f;
+    //public float gravity = 20f;
+    //public int horizontal;
+    //private Vector3 moveDirection = Vector3.zero;
+
+    //void Update()
+    //{
+    //    CharacterController controller = GetComponent<CharacterController>();
+
+    //    if (Input.GetKey(KeyCode.A))
+    //        horizontal = -1;
+
+    //    if (Input.GetKey(KeyCode.D))
+    //        horizontal = 1;
+
+    //    if(controller.isGrounded)
+    //    {
+    //        moveDirection = new Vector3(horizontal, 0, 0);
+    //        moveDirection = transform.TransformDirection(moveDirection);
+    //        moveDirection *= speed;
+    //    }
+
+    //    moveDirection.y -= gravity * Time.deltaTime;
+    //    controller.Move(moveDirection * Time.deltaTime);
+    //}
+
+    public float speed = 3.0f;
+    public float rotateSpeed = 3.0f;
+    public float gravity = 9.8f;
     private CharacterController cont;
-    private Vector3 simGravity;
+    public GameObject target;
+    private Vector3 direction = Vector3.zero;
+    private float distMag;
+    private Quaternion rot;
+    private float chaseRange = 10f;
 
     void Start()
     {
-        simGravity = new Vector3(0, -9.8f, 0);
+        cont = GetComponent<CharacterController>();
+        target = GameObject.FindWithTag("Player");
     }
 
     void Update()
     {
-        cont = GetComponent<CharacterController>();
-        cont.SimpleMove(simGravity);
+        //find player direction
+        direction = target.transform.position - transform.position;
+        //make it strictly horizontal
+        direction.y = 0;
+        //measure distance
+        distMag = direction.magnitude;
+
+        if (distMag > chaseRange)
+        {
+            //idle state or patrol area?
+            //use waypoints for this
+        }
+
+        else
+        {
+            //chase state
+            rot = Quaternion.LookRotation(direction);
+            //find target direction and rotate gradually to it
+            transform.rotation = Quaternion.Slerp(transform.rotation, rot, rotateSpeed * Time.deltaTime);
+            //cont.SimpleMove(transform.forward * speed);
+            cont.Move(transform.forward * speed);
+
+        }
     }
+
+    //private float speed = 3f;
+    //private float rotateSpeed = 3f;
+    //private CharacterController cont;
+    //private Vector3 simGravity;
+
+    //void Start()
+    //{
+    //    simGravity = new Vector3(0, -9.8f, 0);
+    //}
+
+    //void Update()
+    //{
+    //    cont = GetComponent<CharacterController>();
+    //    cont.SimpleMove(simGravity);
+    //}
 }
+
---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScript : MonoBehaviour
+{
+    public float speed = 3f;
+    public float gravity = 9.8f;
+    public Transform target;    //set target from inspector instead of looking in Update
+    private CharacterController cont;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        cont = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //make sure object does not change position on z-axis
+        Vector3 pos = transform.position;
+        pos.z = 0;
+        transform.position = pos;
+
+        //roate to look at the player
+        transform.LookAt(target.position);
+        transform.Rotate(new Vector3(0, -90, 0), Space.Self);   //correcting the original rotation
+
+        Vector3 moveDirection = target.position - transform.position;
+        //moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection.y -= gravity * Time.deltaTime;
+        cont.Move(moveDirection.normalized * Time.deltaTime * speed);
+        //cont.Move(moveDirection * speed);
+
+
+
+
+
+        //if(cont.isGrounded)
+        //{
+        //    //move towards player
+        //    if (Vector3.Distance(transform.position, target.position) > .01f)
+        //    {
+        //        //move if distance from target is greater than 1
+        //        transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+        //    }
+        //}
+
+        ////move towards player
+        //if (Vector3.Distance(transform.position, target.position) > .01f)
+        //{
+        //    //move if distance from target is greater than 1
+        //    transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+        //}
+
+        //transform.Translate(new Vector3(0,-gravity,0) * Time.deltaTime);
+
+    }
+}
+
+
+
+//using UnityEngine;
+//using System.Collections;
+
+//public class EnemyScript : MonoBehaviour {
+
+//    private Transform target;       //enemy's target
+//    private float moveSpeed = 3;    //movement speed
+//    private float rotateSpeed = 3;  //speed of turning
+//    private float minRange = 10f;
+//    private float maxRange = 10f;
+//    private float stop = 0;
+//    private Transform curTransform; //current transform data of enemy
+
+//    void Awake()
+//    {
+//        curTransform = transform;   //cache transform data for easy access/performance
+//    }
+
+//	void Start()
+//    {
+//        target = GameObject.FindWithTag("Player").transform;    //target player
+//    }
+
+//	void Update ()
+//    {
+//        //rotate to look at player
+//        float distance = Vector3.Distance(curTransform.position, target.position);
+
+//        if (distance <= maxRange && distance >= minRange)
+//        {
+//            curTransform.rotation = Quaternion.Slerp(curTransform.rotation, Quaternion.LookRotation(target.position - curTransform.position), rotateSpeed * Time.deltaTime);
+//        }
+
+//        else if (distance <= minRange && distance > stop)
+//        {
+//            //move towards player
+//            curTransform.rotation = Quaternion.Slerp(curTransform.rotation, Quaternion.LookRotation(target.position - curTransform.position), rotateSpeed * Time.deltaTime);
+//            curTransform.position += curTransform.forward * moveSpeed * Time.deltaTime;
+//        }
+
+//        else if (distance <= stop)
+//        {
+//            curTransform.rotation = Quaternion.Slerp(curTransform.rotation, Quaternion.LookRotation(target.position - curTransform.position), rotateSpeed * Time.deltaTime);
+//        }
+//	}
+//}
